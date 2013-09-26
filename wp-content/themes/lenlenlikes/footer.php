ab3e155cb222c82dce275c51ed9087d8efe9521d
roberto_arbeit <robertoceschi>
2013-09-26 15:13:45 +0200

    first commit

---
+++ wp-content/themes/lenlenlikes/footer.php
@@ -0,0 +1,15 @@
+        </div>  <!--End Main Content-->
+    </div>  <!--End Inner Container-->
+</div>
+        <!--End Fluid Container-->
+
+        <div id="footer">
+            2013 Len-Len Likes
+        </div>
+
+<?php wp_footer(); ?>
+
+
+</body>
+
+</html> <!-- end page. what a ride! -->
