132fddf06379c3dcf1e6008d9f59c081751cfa54
roberto_arbeit <robertoceschi>
2013-10-04 13:03:22 +0200

    slideshow works

--- wp-content/themes/lenlenlikes/index.php
+++ wp-content/themes/lenlenlikes/index.php
@@ -1,15 +1,15 @@
 <?php
-    /**
-     * The main template file. =>Default Template im Backend
-     *
-     * This is the most generic template file in a WordPress theme
-     * and one of the two required files for a theme (the other being style.css).
-     * It is used to display a page when nothing more specific matches a query.
-     * For example, it puts together the home page when no home.php file exists.
-     *
-     * Learn more: http://codex.wordpress.org/Template_Hierarchy
-     *
-     */
+/**
+ * The main template file. =>Default Template im Backend
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ */
 
 get_header(); ?>
 <?php
